3714c927bcbf59f77924f48ee49b8d08811d5263
Kota Saito <max.k.saito@gmail.com>
2015-10-09 16:50:39 +0900

    FizzBuzz問題

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
@@ -5,6 +5,13 @@ package com.github.study.FizzBuzz
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
-
+    val str =
+      (1 to 100).map(x => (x % 3, x % 5) match {
+        case (0, 0) => "FizzBuzz"
+        case (0, _) => "Fizz"
+        case (_, 0) => "Buzz"
+        case _ => x
+      }).mkString(",")
+    println(str)
   }
 }
