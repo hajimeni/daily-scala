7a92e0a8710890af3ee4c405207915fbda5290a5
Takeshi Sonoda <sonoda@bizreach.co.jp>
2015-10-13 17:27:33 +0900

    FizzBuzz
    複数行ですが。動きました

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
@@ -6,5 +6,14 @@ package com.github.study.FizzBuzz
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
 
+    val f = (num:Int) => {
+      (num % 3 == 0,num % 5 == 0) match {
+        case (true, true)  => "FizzBuzz"
+        case (true, false) => "Fizz"
+        case (false,true)  => "Buzz"
+        case _             => num.toString
+      }
+    }
+    (1 to 100).foreach(num => println( f(num) ) )
   }
 }
