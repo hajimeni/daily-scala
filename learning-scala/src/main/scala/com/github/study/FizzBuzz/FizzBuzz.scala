185cccdd9b23a5b515ec0ef78451fbc08978d7cb
角谷 文康 <fumiyasu.sumiya@bizreach.co.jp>
2015-10-15 10:09:37 +0900

    fizz buzz

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
@@ -5,6 +5,32 @@ package com.github.study.FizzBuzz
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
+    /*
+1 から 100まで
+3の倍数: Fizz
+5の倍数: Buzz
+3の倍数かつ５の倍数: FizzBuzz
+それ以外 はその数字（1,2など）を出力する
+    * */
+    val s = (1 to 100).filterNot(_ % 2 == 0).map(x => {
+      x match {
+        case i if i % 15 == 0 => "FizzBuzz"
+        case i if i % 3 == 0 => "Fizz"
+        case i if i % 5 == 0 => "Buzz"
+        case i => i
+      }
+    }).mkString(",")
+    println(s)
+
+    val i = (1 to 100).filterNot(_ % 2 == 0).map(x => {
+      x match {
+        case i if i % 15 == 0 => "FizzBuzz"
+        case i if i % 3 == 0 => "Fizz"
+        case i if i % 5 == 0 => "Buzz"
+        case i => i
+      }
+    })
+
 
   }
 }
