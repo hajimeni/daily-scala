2bd9e10dcc46046257acefc87ca2d110dc58d377
角谷 文康 <fumiyasu.sumiya@bizreach.co.jp>
2015-10-16 10:34:13 +0900

    nnpを5まで解いてみた

--- learning-scala/src/main/scala/com/github/study/nnp/NNP10.scala
+++ learning-scala/src/main/scala/com/github/study/nnp/NNP10.scala
@@ -7,28 +7,28 @@ trait NNP10 {
 
   // P01 (*) Find the last element of a list.
   def last(list: List[Int]): Int = {
-    ???
+    list.last
   }
 
   // P02 (*) Find the last but one element of a list.
   def penultimate(list: List[Int]): Int = {
-    ???
+    list.reverse.tail.head
   }
 
   def nth(n: Int, list: List[Int]): Int = {
-    ???
+    list.drop(n).head
   }
 
   def length(list: List[Int]): Int = {
-    ???
+    list.length
   }
 
   def reverse(list: List[Int]): List[Int] = {
-    ???
+    list.reverse
   }
 
   def isPalindrome(list: List[Int]): Boolean = {
-    ???
+    list == list.reverse
   }
 
   def flatten(nested: List[Any]): List[Any] = {
