57e70769d1fee79e8ce5e8cc2a2b3887f77430b8
keisuke.sekiguchi <keisuke.sekiguchi@bizreach.co.jp>
2015-10-22 14:20:09 +0900

    day06

--- learning-scala/src/main/scala/com/github/study/nnp/NNP10.scala
+++ learning-scala/src/main/scala/com/github/study/nnp/NNP10.scala
@@ -12,6 +12,7 @@ trait NNP10 {
     @tailrec
     def last_(list: List[Int]): Int = {
       list match{
+        case Nil => throw new IllegalArgumentException
         case head :: Nil => head
         case head :: tail => last_(tail)
       }
@@ -24,6 +25,7 @@ trait NNP10 {
     @tailrec
     def penultimate_(list: List[Int]): Int = {
       list match{
+        case Nil => throw new IllegalArgumentException
         case head :: last :: Nil => head
         case head :: tail => penultimate_(tail)
       }
@@ -33,20 +35,21 @@ trait NNP10 {
 
   def nth(n: Int, list: List[Int]): Int = {
     @tailrec
-    def nth_(acc: Int, list: List[Int]): Int = {
-      acc match{
-        case 0 => list.head
-        case _ => nth_(acc - 1, list.tail)
+    def nth_(list: List[Int]): Int = {
+      list match{
+        case Nil => throw new IllegalArgumentException
+        case head :: Nil => head
+        case head :: tail => nth_(tail)
       }
     }
-    nth_(n, list)
+    nth_(list.view(0, n + 1).toList)
   }
 
   def length(list: List[Int]): Int = {
     @tailrec
     def length_(acc: Int, list: List[Int]): Int = {
       list match{
-        case head :: tail  => length_(acc + 1, list.tail)
+        case head :: tail  => length_(acc + 1, tail)
         case Nil => acc
       }
     }
@@ -57,7 +60,7 @@ trait NNP10 {
     @tailrec
     def reverse_(acc: List[Int], list: List[Int]): List[Int] = {
       list match{
-        case head :: tail  => reverse_(head :: acc, list.tail)
+        case head :: tail  => reverse_(head :: acc, tail)
         case Nil => acc
       }
     }
@@ -65,23 +68,63 @@ trait NNP10 {
   }
 
   def isPalindrome(list: List[Int]): Boolean = {
-    ???
+    @tailrec
+    def isPalindrome_(list: List[Int]): Boolean = {
+      list match{
+        case Nil => throw new IllegalArgumentException
+        case head :: Nil => true
+        case head :: tail if head == tail.reverse.head  => isPalindrome_(tail.reverse.tail)
+      }
+    }
+    isPalindrome_(list)
   }
 
-  def flatten(nested: List[Any]): List[Any] = {
-    ???
+  def flatten(nested: => List[Any]): List[Any] = {
+    @tailrec
+    def flatten_(acc: => List[Any], list: List[Any]): List[Any] = {
+      list match{
+        case (head: Int) :: tail => flatten_(acc :+ head, tail)
+        case (head: List[_]) :: tail => flatten_(acc ::: flatten(head), tail)
+        case head :: tail => throw new IllegalArgumentException
+        case Nil => acc
+      }
+    }
+    flatten_(Nil, nested)
   }
 
   def compress(list: List[Symbol]): List[Symbol] = {
-    ???
+    @tailrec
+    def compress_(acc: List[Symbol], list: List[Symbol]): List[Symbol] = {
+      list match{
+        case head :: tail if acc.nonEmpty && head == acc.head  => compress_(acc, tail)
+        case head :: tail => compress_(head :: acc, tail)
+        case Nil => acc.reverse
+      }
+    }
+    compress_(Nil, list)
   }
 
   def pack(list: List[Symbol]): List[List[Symbol]] = {
-    ???
+    @tailrec
+    def pack_(acc: List[List[Symbol]], tmpAcc: List[Symbol], list: List[Symbol]): List[List[Symbol]] = {
+      list match{
+        case head :: tail if tmpAcc.nonEmpty && head != tmpAcc.head  => pack_(tmpAcc :: acc, head :: Nil, tail)
+        case head :: tail => pack_(acc, head :: tmpAcc, tail)
+        case Nil => (tmpAcc :: acc).reverse
+      }
+    }
+    pack_(Nil, Nil, list)
   }
 
   def encode(list: List[Symbol]): List[(Int, Symbol)] = {
-    ???
+    @tailrec
+    def encode_(acc: List[(Int, Symbol)], list: List[Symbol], tmpAcc: Option[(Int, Symbol)] = None): List[(Int, Symbol)] = {
+      list match{
+        case head :: tail if tmpAcc.nonEmpty && Option(head) != tmpAcc.map(_._2) => encode_(tmpAcc.get :: acc, tail, Option((1, head)))
+        case head :: tail => encode_(acc, tail, tmpAcc.map(t => (t._1 + 1, head)).orElse(Option((1, head))))
+        case Nil => tmpAcc.map(t => (t :: acc).reverse).getOrElse(Nil)
+      }
+    }
+    encode_(Nil, list)
   }
-
 }
