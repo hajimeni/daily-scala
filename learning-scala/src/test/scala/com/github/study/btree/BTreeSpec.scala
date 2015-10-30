8a735fb77f86e76f3c60e61f7193b9a4110f504d
Shintaro Tamaki <shintaro.tamaki@bizreach.co.jp>
2015-10-30 10:29:22 +0900

    Update BTreeSpec.scala

--- learning-scala/src/test/scala/com/github/study/btree/BTreeSpec.scala
+++ learning-scala/src/test/scala/com/github/study/btree/BTreeSpec.scala
@@ -23,7 +23,36 @@ class BTreeSpec extends FunSpec with Matchers {
         bTree2.size should be (3)
         bTree3.size should be (7)
       }
-    }
+      
+      it("should compute max value of BTree") {
+        bTree1.max should be (1)
+        bTree2.max should be (3)
+        bTree3.max should be (7)
+      }
 
+      it("should compute min value of BTree") {
+        bTree1.min should be (1)
+        bTree2.min should be (1)
+        bTree3.min should be (1)
+      }
+
+      it("should compute sum value of BTree") {
+        bTree1.sum should be (1)
+        bTree2.sum should be (6)
+        bTree3.sum should be (28)
+      }
+
+      it("should compute avg value of BTree") {
+        bTree1.avg should be (1)
+        bTree2.avg should be (2)
+        bTree3.avg should be (4)
+      }
+
+      it("should create BTree from list") {
+        BTree(List(1)) should be (bTree1)
+        BTree(List(1, 2, 3)) should be (bTree2)
+        BTree(List(1, 2, 3, 4, 5, 6, 7)) should be (bTree3)
+      }
+    }
   }
 }
