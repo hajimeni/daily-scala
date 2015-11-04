bfb233e58ee1e311bd62dedf8487e5e1e43f923b
Kota Saito <max.k.saito@gmail.com>
2015-11-04 10:22:37 +0900

    btree

--- learning-scala/src/main/scala/com/github/study/btree/BTree.scala
+++ learning-scala/src/main/scala/com/github/study/btree/BTree.scala
@@ -1,20 +1,35 @@
 package com.github.study.btree
 
-/**
- * Created by shintaro.tamaki on 2015/10/29.
- */
 sealed trait Node {
   def size: Int
+  def max: Int
+  def min: Int
+  def avg: Int
+}
+
+case class Branch(left: Node, value: Int, right: Node) extends Node {
+  override def size: Int = left.size + 1 + right.size
+
+  override def max: Int = List[Int](left.max, value, right.max).max
+
+  override def min: Int = List[Int](left.min, value, right.min).min
+
+  override def avg: Double = (left.avg + value + right.avg) / 3
 }
 
 case class Leaf(value: Int) extends Node {
-  def size: Int = 1
-}
+  override def size: Int = 1
 
-case class Branch(left:  Node, value: Int, right: Node) extends Node {
-  def size: Int = left.size + 1 + right.size
+  override def max: Int = value
+
+  override def min: Int = value
+
+  override def avg: Int = value
 }
 
 case class BTree(node: Node) {
+  def max: Int = node.max
+  def min: Int = node.min
   def size: Int = node.size
+  def avg: Int = node.avg
 }
