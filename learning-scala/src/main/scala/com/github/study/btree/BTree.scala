7d9a25dd3f9a7c301066418718e6877570e09244
Takeshi Sonoda <sonoda@bizreach.co.jp>
2015-11-05 10:18:38 +0900

    BTree

--- learning-scala/src/main/scala/com/github/study/btree/BTree.scala
+++ learning-scala/src/main/scala/com/github/study/btree/BTree.scala
@@ -5,16 +5,50 @@ package com.github.study.btree
  */
 sealed trait Node {
   def size: Int
+  def max: Int
+  def value:Int
+  def min:Int
+  def sum:Int
+  def ave: Double = {this.sum/this.size}
+  def find(v:Int):Option[Node]
+
+  def max_(a:Int,b:Int):Int ={
+    if(a > b) a else b
+  }
+  def min_(a:Int,b:Int):Int ={
+    if(a < b) a else b
+  }
+
 }
 
 case class Leaf(value: Int) extends Node {
   def size: Int = 1
+  def max:Int  = value
+  def min:Int  = value
+  def sum:Int  = value
+  def find(v:Int):Option[Node]={if(v==value) Some(this) else None}
+
 }
 
 case class Branch(left:  Node, value: Int, right: Node) extends Node {
   def size: Int = left.size + 1 + right.size
+  def max:Int =  max_(value,max_(left.max,right.max))
+  def min:Int =  min_(value,min_(left.min,right.min))
+  def sum:Int = value + left.sum + right.sum
+  def find(v:Int):Option[Node]= {
+    if (v == value) Some(this)
+    else if (v < value && left != None) left.find(v)
+    else if (v > value && left != right) right.find(v)
+    else None
+  }
 }
 
 case class BTree(node: Node) {
   def size: Int = node.size
+  def max: Int = node.max
+  def min: Int = node.min
+  def sum: Int = node.sum
+  def ave: Double = {this.sum/this.size}
+  def find(v:Int):Option[Node]=node.find(v)
+
 }
--- learning-scala/src/test/scala/com/github/study/btree/BTreeSpec.scala
+++ learning-scala/src/test/scala/com/github/study/btree/BTreeSpec.scala
@@ -23,6 +23,37 @@ class BTreeSpec extends FunSpec with Matchers {
         bTree2.size should be (3)
         bTree3.size should be (7)
       }
+
+      it("Max Test"){
+        bTree1.max should be (1)
+        bTree2.max should be (3)
+        bTree3.max should be (7)
+      }
+
+      it("Min Test"){
+        bTree1.min should be (1)
+        bTree2.min should be (1)
+        bTree3.min should be (1)
+      }
+
+      it("Sum Test"){
+        bTree1.sum should be (1)
+        bTree2.sum should be (6)
+        bTree3.sum should be (28)
+      }
+
+      it("Sum AVG"){
+        bTree1.ave should be (1)
+        bTree2.ave should be (2)
+        bTree3.ave should be (4)
+      }
+
+      it("Find"){
+        bTree1.find(1) should be (Some(Leaf(1)))
+        bTree2.find(2) should be (Some(Branch(Leaf(1),2,Leaf(3))))
+        bTree3.find(6) should be (Some(Branch(Leaf(5),6,Leaf(7))))
+        bTree3.find(7) should be (Some(Leaf(7)))
+      }
     }
 
   }
