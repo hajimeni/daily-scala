ad55d0290fb4b54145e078ea7bac70ba3a979e5d
Shintaro Tamaki <shintaro.tamaki@bizreach.co.jp>
2015-12-16 20:27:59 +0900

    Create RationalSpec.scala

---
+++ learning-scala/src/test/scala/com/github/study/rational/RationalSpec.scala
@@ -0,0 +1,28 @@
+package com.github.study.rational
+
+import org.scalatest._
+
+class RationalSpec extends FunSpec with Matchers {
+
+  describe("Rational") {
+    describe("+") {
+      it("should returns new rational") {
+        val x = Rational(1, 3)
+        val y = Rational(1, 3)
+        x + y should be (Rational(2, 3))
+      }
+    }
+    
+    describe("compare") {
+      it("should compare rational") {
+        val x = Rational(1, 3)
+        val y = Rational(1, 2)
+        println(x, y)
+        println(x < y)
+        println(x > y)
+        println(x <= y)
+        println(x >= y)
+      }
+    }
+  }
+}
