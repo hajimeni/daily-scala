6ab870e746b3529b0a61fd9c27fb28f439213412
安藤 絵里花 <erika.ando@bizreach.co.jp>
2018-02-02 11:21:05 +0900

    ando_day1_wordCount

--- learning-scala/src/main/scala/com/github/study/wordcount/WordCount.scala
+++ learning-scala/src/main/scala/com/github/study/wordcount/WordCount.scala
@@ -10,7 +10,16 @@ class WordCount {
   // https://gist.github.com/j5ik2o/7210762
   //------------------------------------------------------
   def countFruitsFromLines(lines: List[String]): Map[String, Int] = {
-    ???
+    val fruitList = lines.flatMap(_.split(" "))
+    fruitList.groupBy(identity).mapValues(_.size)
+  }
+
+  val lines = List("apple banana", "orange apple mango", "kiwi papaya orange","mango orange muscat apple")
+  val fruitsCounts = countFruitsFromLines(lines)
+  if (fruitsCounts == Map("banana" -> 1, "muscat" -> 1, "orange" -> 3, "mango" -> 2, "apple" -> 3, "kiwi" -> 1, "papaya" -> 1)){
+    println("正解")
+  } else {
+    println("不正解")
   }
 
 //  /**
