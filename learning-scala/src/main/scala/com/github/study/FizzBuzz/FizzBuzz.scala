306ce22f51a7f995152639678510063a99d2bb50
KotaroNishiyamaBiz <kotaro.nishiyama@bizreach.co.jp>
2018-02-07 10:08:54 +0900

    FizzBuzz実装

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
@@ -5,6 +5,8 @@ package com.github.study.FizzBuzz
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
-
+    {1 to 100}
+        .collect { case i if i % 15 == 0 => "FizzBuzz"; case i if i % 5 == 0 => "Buzz" case i if i % 3 == 0 => "Fizz" case i => i}
+        .foreach(i => println(i))
   }
 }
