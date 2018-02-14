898ed4028f23efe967198680b62b80085e0a3331
KotaroNishiyamaBiz <kotaro.nishiyama@bizreach.co.jp>
2018-02-14 10:03:01 +0900

    s-99 5まで

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
@@ -5,8 +5,55 @@ package com.github.study.FizzBuzz
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
-    {1 to 100}
-        .collect { case i if i % 15 == 0 => "FizzBuzz"; case i if i % 5 == 0 => "Buzz" case i if i % 3 == 0 => "Fizz" case i => i}
-        .foreach(i => println(i))
+
+//    // その2
+//    (1 to 100).map { case i if i % 15 == 0 => "FizzBuzz" case i if i % 5 == 0 => "Buzz" case i if i % 3 == 0 => "Fizz" case i => i.toString}.foreach(i => println(i))
+//
+//    // その3
+//    (1 to 100)
+//      .filter(_ % 2 != 0)
+//      .collect {
+//        case i if i % 15 == 0 => "FizzBuzz"
+//        case i if i % 5 == 0 => "Buzz"
+//        case i if i % 3 == 0 => "Fizz"
+//        case i if i == 1 => i.toString
+//      }.foreach(println)
+//
+//    // その4
+//    println((1 to 100)
+//      .map {
+//        case i if i % 15 == 0 => "FizzBuzz"
+//        case i if i % 5 == 0 => "Buzz"
+//        case i if i % 3 == 0 => "Fizz"
+//        case i => i.toString
+//      }.mkString(","))
+
+    // その5
+    println((1 to 100)
+      .map {
+        case i if i % 15 == 0 => "FizzBuzz"
+        case i if i % 5 == 0 => "Buzz"
+        case i if i % 3 == 0 => "Fizz"
+        case i => i.toString
+      }.filter(i => i != "Fizz" && i != "Buzz" && i != "FizzBuzz")
+      .map(i => i.toInt)
+      .sum)
+
+        // その6
+//    val fizzList = List(0,0,1)
+//    val BuzzList = List(0,0,0,0,1)
+//    fizzList.last
+
+    def last(list: List[Object]): Object = {
+      list.last
+    }
+
+    def penultimate(list: List[Object]): Object = {
+      list.reverse(1)
+    }
+
+    def nth(index: Int, list:List[Object]): Object = {
+      list(index)
+    }
   }
 }
--- learning-scala/src/main/scala/com/github/study/nnp/NNP10.scala
+++ learning-scala/src/main/scala/com/github/study/nnp/NNP10.scala
@@ -7,28 +7,49 @@ trait NNP10 {
 
   // P01 (*) Find the last element of a list.
   def last(list: List[Int]): Int = {
-    ???
+    list match {
+      case x :: Nil => x
+      case _ :: y => last(y)
+    }
   }
 
   // P02 (*) Find the last but one element of a list.
   def penultimate(list: List[Int]): Int = {
-    ???
+    list match {
+      case x :: _ :: Nil => x
+      case _ :: y :: z => penultimate(y :: z)
+    }
   }
 
   def nth(n: Int, list: List[Int]): Int = {
-    ???
+    list match {
+      case x :: _ if index == 0 => x
+      case _ :: y if index > 0 => nth(index - 1, y)
+    }
   }
 
   def length(list: List[Int]): Int = {
-    ???
+    def count(list: List[Int], acc: Int): Int = {
+      list match {
+        case _ :: Nil => acc + 1
+        case _ :: y => count(y,acc+1)
+      }
+    }
+    count(list, 0)
   }
 
   def reverse(list: List[Int]): List[Int] = {
-    ???
+    def createReverse(list: List[Int], acc: List[Int]): List[Int] = {
+      list match {
+        case x :: Nil => x :: acc
+        case x :: y => createReverse(y, x :: acc)
+      }
+    }
+    createReverse(list, nil)
   }
 
   def isPalindrome(list: List[Int]): Boolean = {
-    ???
+    reverse(list) == list
   }
 
   def flatten(nested: List[Any]): List[Any] = {
@@ -46,5 +67,4 @@ trait NNP10 {
   def encode(list: List[Symbol]): List[(Int, Symbol)] = {
     ???
   }
-
 }
